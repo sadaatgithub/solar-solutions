e83296a9c200676f4c4a879f919657dfc9150362
sadaatgithub <sadachaudhari@gmail.com>
2023-06-11 16:30:40 +0530

    illustrations added

--- app/services/[slug]/page.js
+++ app/services/[slug]/page.js
@@ -43,20 +43,20 @@ const services = [
         heading:"System Integration and Optimization",
         desc:`We go beyond installation by seamlessly integrating your solar system with your existing electrical infrastructure. Our team ensures that the system is properly 
         connected and synchronized to maximize energy efficiency and minimize any potential disruptions. We conduct thorough testing and quality checks to guarantee optimal system performance.`,
-        img:"/solarVEctor.jpg",
+        img:"/system-animate.svg",
       },
       {
         id:4,
         heading:"Maintenance and Monitoring",
         desc:`To ensure your solar system operates at peak performance throughout its lifespan, we offer comprehensive maintenance and monitoring services.
          Our technicians conduct regular inspections, cleaning, and system optimization to identify and address any potential issues. Through advanced monitoring systems, we track the performance of your solar system and promptly respond to any deviations or anomalies.`,
-         img:"/solarVEctor.jpg",
+         img:"/maintenance-animate.svg",
       },
       {
         id:5,
         heading:"Customer Support",
         desc:`At Solar Solutions, we prioritize customer satisfaction. Our dedicated customer support team is available to address any questions or concerns you may have throughout the entire solar work process. We strive to provide clear communication, timely assistance, and ongoing support to ensure your complete satisfaction.`,
-        img:"/solarVEctor.jpg",
+        img:"/support-animate.svg",
 
       },
       
@@ -78,26 +78,26 @@ const services = [
         id:1,
         heading:"HT Services",
         desc:`Our HT services cater to projects that require high voltage transmission and distribution. With our expertise in HT solar installations, we can design, engineer, and implement solar solutions for utility-scale projects, commercial installations, and other applications that demand higher voltage levels. Our team ensures compliance with safety regulations and employs specialized equipment to guarantee reliable and efficient operation of HT solar systems.`,
-       img:"/solarVEctor.jpg",
+       img:"/electrician-animate.svg",
       },
       {
         id:2,
         heading:"LT Services",
         desc:`For residential and small to medium-sized commercial projects, our LT services are designed to meet the specific energy requirements of individual customers. We offer professional design, installation, and maintenance services for LT solar systems. Our team works closely with you to develop a customized solution that maximizes energy production, optimizes cost savings, and integrates seamlessly with your existing electrical infrastructure.`,
-        img:"/solarVEctor.jpg",
+        img:"/electrician-animate-2.svg",
       },
       {
         id:3,
         heading:"Expertise and Safety",
         desc:`With our in-depth knowledge and experience in both HT and LT solar systems, we prioritize safety and adhere to industry best practices. Our qualified professionals are well-versed in the intricacies of working with different voltage levels and follow rigorous safety protocols throughout the project lifecycle. You can trust us to ensure the safe and reliable operation of your HT or LT solar system.`,
-        img:"/solarVEctor.jpg",
+        img:"/safety-animate.svg",
       },
       {
         id:4,
         heading:"Quality Assurance and Support",
         desc:`At Solar Solutions, we are committed to delivering high-quality HT and LT services. We use top-of-the-line equipment and components, sourced from trusted manufacturers,
          to ensure the longevity and performance of your solar system. Additionally, our customer support team is always available to address any questions or concerns you may have, providing ongoing assistance and guidance.`,
-         img:"/solarVEctor.jpg",
+         img:"/support-animate.svg",
       }
     ],
     footerHeading:`By offering comprehensive HT and LT services, we aim to meet the diverse needs of our customers across a wide range of solar projects. Whether it's a large-scale utility installation or a residential system, we have the expertise and resources to deliver reliable, efficient, and cost-effective solutions. Contact us today to discuss your HT or LT solar requirements and let us help you harness the power of solar energy.`,
@@ -121,30 +121,35 @@ const services = [
         id:2,
         heading:"Customized Solar Solutions",
         desc:`Based on the assessment, our consultants will work closely with you to develop a customized solar solution that aligns with your goals, preferences, and budget. We take into consideration your energy needs, financial objectives, and any specific requirements you may have to ensure that the proposed solar system meets your expectations.`,
+        img:"/customization-animate.svg"
         
       },
       {
         id:3,
         heading:"Financial Analysis",
         desc:`As part of our solar consultation service, we provide a detailed financial analysis that outlines the potential cost savings and return on investment (ROI) associated with your solar project.
-         Our experts will assess available incentives, financing options, and utility policies to provide you with a clear understanding of the financial benefits of going solar`
+         Our experts will assess available incentives, financing options, and utility policies to provide you with a clear understanding of the financial benefits of going solar`,
+         img:"/consulting-animate.svg"
       },
       {
         id:4,
         heading:"Regulatory and Permitting Assistance",
         desc:`Navigating the regulatory landscape and obtaining the necessary permits for solar installations can be complex. Our consultants are well-versed in local regulations and will
-         guide you through the entire permitting process, ensuring compliance with all relevant codes and regulations.`
+         guide you through the entire permitting process, ensuring compliance with all relevant codes and regulations.`,
+          img:"/consulting-animate.svg"
       },
       {
         id:5,
         heading:"Technology and Equipment Recommendations",
         desc:`With our extensive knowledge of solar technologies and equipment, we will recommend the most suitable solar panels, 
-        inverters, and other components for your project. We partner with reputable manufacturers to provide high-quality, reliable, and efficient equipment that meets your specific requirements.`
+        inverters, and other components for your project. We partner with reputable manufacturers to provide high-quality, reliable, and efficient equipment that meets your specific requirements.`,
+        img:"/support-animate.svg"
       },
       {
         id:6,
         heading:"Ongoing Support",
-        desc:`Our commitment to our customers extends beyond the initial consultation. We offer ongoing support throughout the entire solar journey, from the initial consultation to post-installation maintenance and monitoring. Our team is always available to answer any questions, address concerns, and provide ongoing assistance to ensure your solar system operates optimally.`
+        desc:`Our commitment to our customers extends beyond the initial consultation. We offer ongoing support throughout the entire solar journey, from the initial consultation to post-installation maintenance and monitoring. Our team is always available to answer any questions, address concerns, and provide ongoing assistance to ensure your solar system operates optimally.`,
+        img:"/support-animate.svg"
       }
     ],
     footerHeading:`By choosing our solar consultation service, you can benefit from our expertise, industry insights, and personalized approach. We aim to empower you with the knowledge and confidence to make informed decisions about your solar energy project. Contact us today to schedule a solar consultation and take the first step toward a sustainable energy future.`,
@@ -189,6 +194,10 @@ const Page = () => {
               <h2 className="md:text-5xl text-3xl  font-bold text-white capitalize">Start your saving now...!</h2>
               <Button text="Get in Touch" className="bg-white cursor-pointer"/>
               
+            </div>
+            <div className="flex flex-col">
+            <span className="text-sm text-gray-500 italic underline"><a href="https://storyset.com/business">Business illustrations by Storyset</a></span>
+            <span className="text-sm text-gray-500 italic underline"><a href="https://storyset.com/worker">Worker illustrations by Storyset</a></span>
             </div>
         </div>
       ))}
