a9c4d8102203d496bfaeba18db21668af6fd6287
sadaatgithub <sadachaudhari@gmail.com>
2023-06-18 12:00:36 +0530

    code refactor

--- app/components/RecentProjects/RecentProject.js
+++ app/components/RecentProjects/RecentProject.js
@@ -1,31 +1,40 @@
-'use client'
-import React from 'react'
-import config from "../../../app/index.json"
-
-import TopSubHeading from '../common/heading/TopSubHeading'
-import DivTitle from '../common/heading/DivTitle'
-import { motion as m } from 'framer-motion'
-import { fadeIn } from '@/lib/animation'
-import Project from '../Project'
+"use client";
+import React from "react";
+import config from "../../../app/index.json";
+import TopSubHeading from "../common/heading/TopSubHeading";
+import DivTitle from "../common/heading/DivTitle";
+import { motion as m } from "framer-motion";
+import { fadeIn } from "@/lib/animation";
+import Project from "../Project";
 
 const RecentProject = () => {
-    const projects = config.projects
+  const projects = [...config.projects];
   return (
     <div className="flex flex-col gap-20 py-20">
-        <div className="flex flex-col gap-4 px-4 lg:px-20 items-center">
-           <TopSubHeading text="What We Do"/>
-            <DivTitle text="Recent Projects"/>
-        </div>
+      <div className="flex flex-col gap-4 px-4 lg:px-20 items-center">
+        <TopSubHeading text="What We Do" />
+        <DivTitle text="Recent Projects" />
+      </div>
 
-<div className="grid lg:grid-cols-4 grid-cols-1 sm:grid-cols-2 place-items-center">
-    {projects.slice(0,4).map((project,idx) => 
-        <m.div className="" key={project.id} variants={fadeIn('up','tween',idx*0.3,0.5)} initial="hidden"  whileInView="show" viewport={{once:true, amount:0.5}}>
-          <Project project={project}/>
-        </m.div>
-    )}
-</div>
+      <div className="grid lg:grid-cols-4 grid-cols-1 sm:grid-cols-2 place-items-center">
+        {projects
+          .sort((a, b) => b.capacity - a.capacity)
+          .slice(0, 4)
+          .map((project, idx) => (
+            <m.div
+              className=""
+              key={project.id}
+              variants={fadeIn("up", "tween", idx * 0.3, 0.3)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: true, amount: 0.3 }}
+            >
+              <Project project={project} />
+            </m.div>
+          ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RecentProject
+export default RecentProject;
